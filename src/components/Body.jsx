42054ea38fcb4da0ab359971c179f19a5ceb4017
Shahzaibshzy <112001025+Shahzaibshzy@users.noreply.github.com>
2024-11-05 02:28:37 +0500

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -42,7 +42,7 @@ const Body = () => {
           <div className="inline-block relative group">
               <button className="text-gray-800 text-lg hover:text-custom-blue font-bold">
               Tools
-              <i className=" px-3 fas fa-chevron-down"></i>
+              <i className=" fas fa-chevron-down"></i>
             </button>
             {/* Dropdown menu */}
             <ul className="absolute hidden group-hover:block
