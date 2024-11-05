d8b229d84fd2d6383f6a6def7ddb4fde880211b1
Shahzaibshzy <112001025+Shahzaibshzy@users.noreply.github.com>
2024-11-06 02:22:42 +0500

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,10 @@ import sections from "../resources/sections";
 
 const Card = () => {
   return (
-    <div className="trainingarea sp_bottom_20 -mt-20 sm:mt-10 md:mt-20 lg:-mt-28" data-aos="fade-up">
+    <div
+      className="trainingarea sp_bottom_20 -mt-20 sm:mt-10 md:mt-20 lg:-mt-28"
+      data-aos="fade-up"
+    >
       <div className="container mx-auto">
         <div className="flex flex-wrap justify-center -mx-3">
           {sections.map((section, index) => (
@@ -15,29 +18,29 @@ const Card = () => {
             >
               <div
                 className={`trainingarea__wrapper bg-white p-6 rounded-xl 
-                  border-[1px] border-gray-200 transition-all duration-200 
-                  ${section.hoverBorder} flex flex-col h-full shadow-md hover:shadow-xl hover:scale-105`}
+              border-[1px] border-gray-200 transition-all duration-200 
+              ${section.hoverBorder} flex flex-col h-full shadow-md hover:shadow-xl hover:scale-105`}
               >
                 <div className="trainingarea__icon flex items-center mb-6 ml-3 mt-2">
                   <div
                     className={`flex items-center justify-center w-10 h-10 rounded-full
-                       ${section.borderColor} mr-4`}
+                  ${section.borderColor} mr-4`}
                   >
                     <i
-                      className={`${section.iconClass} text-3xl  ${section.linkColor} 
-                      ${section.borderColor} `}
+                      className={`${section.iconClass} text-3xl ${section.linkColor} 
+                  ${section.borderColor}`}
                     ></i>
                   </div>
                   <div className="trainingarea__content">
                     <h6 className="text-xl font-semibold mt-2">
                       <a
                         href={section.link}
-                        className={`text-gray-600 hover:underline`}
+                        className="text-gray-600 hover:underline"
                       >
                         {section.title}
                       </a>
                     </h6>
-                    <p className="text-gray-600 ">{section.description}</p>
+                    <p className="text-gray-600">{section.description}</p>
                   </div>
                 </div>
               </div>
