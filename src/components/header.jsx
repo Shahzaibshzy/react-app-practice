0f381ca9e5b36c6cbb6e19a205b8ef9d881cfdd2
Shahzaibshzy <112001025+Shahzaibshzy@users.noreply.github.com>
2024-11-05 20:51:20 +0500

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,40 +3,50 @@ import "tailwindcss/tailwind.css";
 
 const Header = () => {
   return (
-    <header className="hidden md:block">
-      <div className="border-b-0 bg-black h-11 flex items-center justify-between px-4">
-        <span className="text-white text-md ml-16 md:ml-36 ">
-          Call Us: +92-333-7276335 - Mail Us: info@shekztech.com
-        </span>
-        <span className="text-white text-md flex items-center ml-96 md:ml-96">
-          <i className="fas fa-map-marker-alt text-blue-900 mr-2 ml-32 md:ml-52"></i>
-          Plot 1177, Sector 31B - Crossing, Karachi, Sindh
-        </span>
-        <div className="flex space-x-5 mr-44 md:flex-row flex-wrap">
-          <a
-            href="#"
-            className="text-white text-lg transform hover:scale-95 transition-transform duration-200 ease-in-out"
-          >
-            <i className="fab fa-facebook-f hover:text-blue-900 transition-colors duration-200"></i>
-          </a>
-          <a
-            href="#"
-            className="text-white text-lg transform hover:scale-95 transition-transform duration-200 ease-in-out"
-          >
-            <i className="fab fa-linkedin-in hover:text-blue-900 transition-colors duration-200"></i>
-          </a>
-          <a
-            href="#"
-            className="text-white text-lg transform hover:scale-95 transition-transform duration-200 ease-in-out"
-          >
-            <i className="fab fa-instagram hover:text-red-800 transition-colors duration-200"></i>
-          </a>
-          <a
-            href="#"
-            className="text-white text-lg transform hover:scale-95 transition-transform duration-200 ease-in-out"
-          >
-            <i className="fab fa-youtube hover:text-red-600 transition-colors duration-200"></i>
-          </a>
+    <header>
+      <div className=" hidden lg:block bg-black text-white relative h-10">
+        <div className="container mx-auto px-4 sm:px-8 md:px-16 flex items-center space-y-1 justify-between">
+          {/* Left Section */}
+          <div className="topbar__left flex space-x-4">
+          <ul className="space-x-1 text-md  lg:flex">
+              <li>Call Us: +92-333-7276335</li>
+              <li>- Mail Us: info@shekztech.com</li>
+            </ul>
+          </div>
+
+          {/* Right Section */}
+          <div className="topbar__right flex items-center justify-end mb-2 space-x-9 space-y-1">
+            <div className="topbar__icon">
+              <i className="fas fa-map-marker-alt text-blue-900 mr-0 scale-90 mt-2"></i>
+            </div>
+            <div className="topbar__text ml-1">
+              <p>Plot 1177, Sector 31B - Crossing, Karachi, Sindh</p>
+            </div>
+            <div className="topbar__list">
+              <ul className="flex space-x-5">
+                <li>
+                  <a href="https://www.facebook.com/shekztech">
+                    <i className="fab fa-facebook-f hover:text-blue-900 transition-colors duration-200"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/company/shekztech">
+                    <i className="fab fa-linkedin-in hover:text-blue-900 transition-colors duration-200"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/shekztech/">
+                    <i className="fab fa-instagram hover:text-red-800 transition-colors duration-200"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/shakzee">
+                    <i className="fab fa-youtube hover:text-red-600 transition-colors duration-200"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </header>
